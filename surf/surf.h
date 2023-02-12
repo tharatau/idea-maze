4f2cf0ea87ee4afcbf4d92d059d21cf0d89b4604
Ayushman Chhabra <ayushmxn@outlook.com>
2023-02-12 03:33:02 -0500

    surf: add math shared library

--- surf/surf.h
+++
@@ -1,7 +0,0 @@
-// Copyright 2023 The Surf Authors
-
-#ifndef SURF_H
-#define SURF_H
-#endif
-
-int main(int argc, char *argv[]);
---
+++ surf/utility_math.c
@@ -0,0 +1,5 @@
+#include "utility_math.h"
+
+int add(int x, int y) {
+    return x + y;
+}
